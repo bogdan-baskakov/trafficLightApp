3114987ca16e345693d2e8838cab3c6533a8bea6
Bogdan Baskakov <bgdnbskkv@gmail.com>
2019-07-23 17:11:04 +0500

    Initial Commit

---
+++ trafficLightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLightApp
+//
+//  Created by Богдан Баскаков on 23/07/2019.
+//  Copyright © 2019 Bogdan Baskakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
