5f9f07884ee570f8503bfc5eb836a067796b305f
Bogdan Baskakov <bgdnbskkv@gmail.com>
2019-07-24 15:52:57 +0500

    1. Added button functionality

--- trafficLightApp/ViewController.swift
+++ trafficLightApp/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var buttonLable: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.backgroundColor = .red
+        redLight.layer.cornerRadius = 65
+        redLight.alpha = 0.3
+        
+        yellowLight.backgroundColor = .yellow
+        yellowLight.layer.cornerRadius = 65
+        yellowLight.alpha = 0.3
+        
+        greenLight.backgroundColor = .green
+        greenLight.layer.cornerRadius = 65
+        greenLight.alpha = 0.3
+        
+        buttonLable.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        if redLight.alpha == 0.30000001192092896 && yellowLight.alpha == 0.30000001192092896 && greenLight.alpha == 0.30000001192092896 {
+            redLight.alpha = 1
+            buttonLable.setTitle("Next light", for: .normal)
+        } else if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            buttonLable.setTitle("Stop", for: .normal)
+        } else {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            buttonLable.setTitle("Start", for: .normal)
+        }
+    }
 }
 
